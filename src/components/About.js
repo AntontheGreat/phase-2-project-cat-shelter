1075f4ec1ecb2774002ecbaa373a3486b92b37a8
Antonio <antonio18casta@aol.com>
2023-03-21 14:48:13 -0400

    About

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import React from "react";
 function About() {
     return (
         <div id="about">
-            <h1>About me</h1>
+            <h1>About Cat Shelter</h1>
             <p>Cat Shelter is a React SPA for putting cats up for adoption and adopting a cat. To put up a cat for adoption please click the Cats route above and fill out the form.</p>
         </div>
     );
